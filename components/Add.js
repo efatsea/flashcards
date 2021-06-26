59cdad4784cb76807b1563b9e52664095fbab9f8
Eleni Fatsea <efatsea@gmail.com>
2021-06-26 10:44:38 +0300

    create the add deck component. try to save the new deck but there is a problem with expo network. I will try later

--- components/Add.js
+++ components/Add.js
@@ -1,15 +1,76 @@
 import React, { Component } from "react"
-import { View, Text, Stylesheet } from "react-native"
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from "react-native"
+import { background, grey, red } from "../utils/colors"
+import { setData } from "../utils/helpers"
 
 class Add extends Component {
+    state = {
+        title: ""
+    }
+
+    setText = (text) => {
+        this.setState({title: text})
+    }
+
+    onClick = () => {
+        console.log("start")
+        const { title } = this.state
+        console.log(title)
+        console.log("1")
+        setData(title)
+        this.setState({ title: "" })
+        console.log("3")
+           
+        
+
+    }
 
     render() {
+        const { text } = this.state
         return (
-            <View>
-                <Text>Add</Text>
+            <View style={styles.container}>
+                <Text>Add New Deck</Text>
+                <Text>What is the title of the new Deck?</Text>
+                <TextInput
+                    placeholder = "Title"
+                    onChangeText = {text => this.setText(text)}
+                    defaultValue={text}
+                />
+                <TouchableOpacity onPress={this.onClick.bind()}>
+                    <Text>Add Deck</Text>
+                </TouchableOpacity>
+                
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: background,
+        alignItems: "center",
+        marginTop: 30,
+    },
+    decksButton: {
+        alignItems: "center",
+        justifyContent: "center",
+        paddingTop: 40,
+        paddingBottom: 40,
+        paddingLeft: 50,
+        paddingRight: 50,
+        marginLeft: 40,
+        marginRight: 40,
+        marginTop: 20,
+        backgroundColor: grey,
+        height: 45,
+
+    },
+    text: {
+        backgroundColor: red,
+    }
+
+})
 
 export default Add
