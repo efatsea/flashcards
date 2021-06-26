3e386ed712c8b159651f5573209de4b8b9406d4d
Eleni Fatsea <efatsea@gmail.com>
2021-06-26 09:41:19 +0300

    create the deck view and add some style

--- components/Decks.js
+++ components/Decks.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react"
-import { View, Text, Stylesheet } from "react-native"
+import { View, Text, StyleSheet, TouchableOpacity, Platform } from "react-native"
+import { background, grey, red } from "../utils/colors"
 import { fetchDecks, setData, initData } from "../utils/helpers"
 
 class Decks extends Component {
     state = {
-        decker: null
+        decks: null,
+        decksName: null
     }
     componentDidMount () {
         if (this.state.object === null) {
@@ -12,14 +14,16 @@ class Decks extends Component {
                 .then((res) => {
                     fetchDecks()
                         .then((obj) => {
-                            this.setState({decker:Object.keys(obj)})
+                            this.setState({ decks: obj })
+                            this.setState({ decksName: Object.keys(obj) })
                         })
                 })
         }
         else {
             fetchDecks()
-                .then((decker) => {
-                    this.setState({decker:Object.keys(decker)})
+                .then((obj) => {
+                    this.setState({ decks: obj })
+                    this.setState({ decksName: Object.keys(obj) })
                 })
         }
         
@@ -28,15 +32,19 @@ class Decks extends Component {
 
     render(){
         
-        const { decker } = this.state
-        console.log(decker)
+        const { decksName, decks } = this.state
+        console.log(decks)
         return(
-            <View>
+            <View style={styles.container}>
                 <Text>Decks</Text>
-                {decker !== null 
-                    ? decker.map((item) => {
+                {decksName !== null 
+                    ? decksName.map((item) => {
                         return (
-                            <Text key={item}>{item}</Text>
+                            <TouchableOpacity key={item.length} style={styles.decksButton}>
+                                <Text key={item}>{item}</Text>
+                                <Text key={item.length}>{decks[item].questions.length} cards</Text>
+                            </TouchableOpacity>
+                            
                         )
                       })
                     :  null
@@ -46,5 +54,33 @@ class Decks extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: background,
+        alignItems: "center",
+        marginTop: 30,
+    },
+    decksButton: {
+        alignItems: "center",
+        justifyContent: "center",
+        paddingTop: 40,
+        paddingBottom:40,
+        paddingLeft: 50,
+        paddingRight: 50,
+        marginLeft: 40,
+        marginRight: 40,
+        marginTop: 20,
+        backgroundColor: grey,
+        height: 45,
+
+    },
+    text: {
+        backgroundColor: red,
+    }
+
+})
 
 export default Decks
--- utils/colors.js
+++ utils/colors.js
@@ -1,6 +1,6 @@
 export const  red = "#f95950"
 export const black = "#563c50"
 export const blue = "#28559a"
-export const grey = "#5c6773"
+export const grey = "#E8E8E8"
 export const background = "#f8f9fa"
 export const white = "#eee"
